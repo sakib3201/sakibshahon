de8e9c436e54cefd2f42f5d0524e0e32c2fd2795
sakib ahamed shahon <53832890+sakib3201@users.noreply.github.com>
2021-03-16 11:34:09 +0600

    First commit of my portfolio site
    Included necessary css and html files also the file for custom fonts

---
+++ code.js
@@ -0,0 +1,28 @@
+        var card = document.getElementById('Activator');
+        var tl = gsap.timeline({
+          defaults: {
+            ease: "power2.inOut"
+          }
+        })
+
+        var toggle = false;
+
+        tl.to('.custom-icon-menu', {
+          background: 'tomato', //'#3cc1c9ff' #805ad5
+          'borderRadius': '5em 0 0 5em'
+        });
+        tl.to('nav', {
+          'clipPath': 'ellipse(100% 100% at 50% 50%)'
+        }, "-=.5")
+        tl.to('nav i', {
+          opacity: 1,
+          transform: 'translateX(0)',
+          stagger: .05
+        }, "-=.5")
+        tl.pause();
+
+        card.addEventListener('click', () => {
+          toggle = !toggle;
+          if (toggle ? tl.play() : tl.reverse());
+
+        })
